e669c0776d8b8c0b664808d3893db396e65163f8
Pumthad Kaewmongkolsri <pumthad1670@gmail.com>
2024-10-03 13:01:11 +0700

    solver exercise 1-7

--- ex-2.js
+++ ex-2.js
@@ -2,3 +2,12 @@
 let lightBulbStatus = "On";
 
 // Start coding here.
+if (lightBulbStatus === "On") {
+  console.log("Light bulb is on");
+} else if (lightBulbStatus === "Off") {
+  console.log("Light bulb is off");
+} else if (lightBulbStatus === "broken") {
+  console.log("Light bulb is broken");
+} else {
+  console.log("Please choose the correct input");
+}
